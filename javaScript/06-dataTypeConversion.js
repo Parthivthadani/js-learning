1f5befc68e353566605d3416f7d717b7c94ca6d9
Parthivthadani <128155471+Parthivthadani@users.noreply.github.com>
2025-02-08 16:52:21 +0000

    file is added

---
+++ javaScript/06-dataTypeConversion.js
@@ -0,0 +1,39 @@
+let String=null
+
+let convertIntoNumber= Number(String)
+
+// console.log(convertIntoNumber)
+// console.log(typeof(convertIntoNumber))
+
+// String to number
+// "33" -> 33 , number
+// "33abc" -> Nan , number
+// undefined ->NaN , number
+// null -> 0
+
+let number = 1
+
+let convertIntoBoolean = Boolean(number)
+
+// console.log(convertIntoBoolean)
+// console.log(typeof(convertIntoBoolean))
+
+
+// 1 -> true
+// 0 -> false
+
+// {
+//     let String = ""
+//     let convertIntoBoolean = Boolean(String)
+//     console.log(convertIntoBoolean)
+//     console.log(typeof(convertIntoBoolean))
+//     // "" -> false, boolean
+//     // "parthiv" -> true, boolean
+// }
+
+let score = 33;
+
+let convertIntoString = String(score)
+
+console.log(convertIntoString)
+console.log(typeof(convertIntoString))
