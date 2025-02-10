fd0499242bc79790a24e9c4c036640d4d6f74119
Parthivthadani <128155471+Parthivthadani@users.noreply.github.com>
2025-02-10 17:39:13 +0000

    ld

---
+++ solve-10-questionsOnTopics/10-queInString.js
@@ -0,0 +1,10 @@
+/* 1 ) String Declaration and Concatenation
+Write a JavaScript program to create two strings, firstName and lastName, and concatenate them to form a full name. Print the result. */
+
+const firstName = "Parthiv"
+const lastName = "Thadani"
+
+console.log(`your fullName is ${firstName} ${lastName}`)
+
+/*2) String Length
+Write a function getStringLength(str) that takes a string as input and returns its length without using the built-in length property.*/
