a025e6010ce4fe18c21ffba9e24457a0b116372e
Parthivthadani <128155471+Parthivthadani@users.noreply.github.com>
2025-02-10 17:17:22 +0000

    leaned memory allocation

--- javaScript/08-datatypes.js
+++ javaScript/08-datatypes.js
@@ -10,8 +10,8 @@ let value1 = undefined;
 let id = Symbol('123')
 let id1 = Symbol('123')
 
-// console.log(number,bigInt,Str,loggedIn,value,value1,id,id1)
-// console.log(id===id1);
+console.log(number,bigInt,Str,loggedIn,value,value1,id,id1)
+ console.log(id===id1);
 
 //non-primitive(refrenced)-> in this type the datatype send a address of the variable and it effect on original value
 
@@ -28,3 +28,30 @@ const myFunction = function(){
 
 // console.log(arr1,object)
 console.log(typeof(id) )
+
+
+//++++++++++++++++++++++++++++++++++++++++++++ Memory-Allocation +++++++++++++++++++++++++++++++++++++++++++++++++++++++\
+
+// there are two types of memory allocation 1) stack(primitive) 2)heap (non-primitive)
+
+//1)stack :Example
+
+let myYtName="parthiv"
+
+let changeYtname=myYtName
+changeYtname ="Pdking"
+
+console.log(myYtName,changeYtname)
+
+//2)heap : Example
+
+let userOne = {
+    name : "Parthiv",
+    email : "thadani@gmail.com"
+}
+
+let userTwo = userOne
+
+userTwo.email =  "parthiv@gmail.com"
+
+console.log (userOne.email , userTwo.email)
